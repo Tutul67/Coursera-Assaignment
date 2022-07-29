8b6dffb2ad1ee56ac8dab62f67351173428819e4
Tutul67 <67mahmud@gmail.com>
2022-07-29 22:36:53 +0300

    ok

---
+++ Peer Graded Assaignment.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook On Ibm watson Studio
+
+# ## __Md Mahmudur Rahman__
+# #### *  __Current Occupation Refinery Process Field Operator In Kuwait National Petroleum Company.__
+
+# ##### _The Reason To Love and To Put Effort In Data Science is It's outcome in return in the vast domain of Job Sectors Of Today's And Tomorrow's nourishing factors for job creation_
+
+# ### While loop
+
+# In[5]:
+
+
+i = 1
+while i <= 10:
+    print (i)
+    i = i+1
+
+
+# ##### Horizontal ru
+# 
+# hype
+# 
+# ***
+# 
+# Markdown | Less | Pretty
+# --- | --- | ---
+# *still* | `renders` | **nicely**
+# 1 | 2 | 3
+# 
+# > blockquotes are very handy to emulate reply text.
+# 
+# 
+# 
+# 
+# 
